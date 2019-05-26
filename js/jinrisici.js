39a38dd4ace86e8f0894e5dc2c34ba153985ba16
FitzMusk <43529681+FitzMusk@users.noreply.github.com>
2019-05-26 08:50:54 +0800

    Update jinrisici.js

--- js/jinrisici.js
+++ js/jinrisici.js
@@ -1,15 +1,15 @@
 function get_poem (poem_ele, info_ele) {
-  var poem = document.querySelector(poem_ele)
-  var info = document.querySelector(info_ele)
-  var xhr = new XMLHttpRequest()
-  xhr.open('get', '//v2.jinrishici.com/one.json')
-  xhr.withCredentials = true
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      var data = JSON.parse(xhr.responseText)
-      poem.innerHTML = data.data.content
-      info.innerHTML = '【' + data.data.origin.dynasty + '】' + data.data.origin.author + '《' + data.data.origin.title + '》'
-    }
-  }
-  xhr.send()
+var poem = document.querySelector(poem_ele)
+var info = document.querySelector(info_ele)
+var xhr = new XMLHttpRequest()
+xhr.open('get', 'https://v2.jinrishici.com/one.json')
+xhr.withCredentials = true
+xhr.onreadystatechange = function () {
+if (xhr.readyState === 4) {
+var data = JSON.parse(xhr.responseText)
+poem.innerHTML = data.data.content
+info.innerHTML = '【' + data.data.origin.dynasty + '】' + data.data.origin.author + '《' + data.data.origin.title + '》'
+}
+}
+xhr.send()
 }
